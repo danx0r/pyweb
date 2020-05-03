59aa6b5137fa6b5e11e6d532de6bb1b29797a4ec
danx0r <dan.miller@eye0.com>
2020-05-03 14:34:46 -0700

    Mein Furher -- I can walk!

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ pg=[
         {div: [
             {a: "link", 'href': "http://www.busyboxes.org", 'style': "font-family: sans; background-color: pink"}
         ]},
-        {span: "goodbye"},
+        {span: "goodbye", 'style': "font-size: 200%"},
         {"button": "OK", "onclick": "alert('okay')"}                                    #regular string is fine too
     ]}
 ]
