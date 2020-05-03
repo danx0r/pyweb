cdf337f995a35e14c9220181873eafc19f0def96
danx0r <dan.miller@eye0.com>
2020-05-03 16:09:55 -0700

    looks promising!

--- pyweb.py
+++ pyweb.py
@@ -3,6 +3,7 @@ import html_tags
 html_tags = [x for x in dir(html_tags) if x[0] != '_']
 from html_tags import *                     #for PyCharm
 
+INDENT=3
 
 def get_tag(e):
     for k in e.keys():
@@ -10,17 +11,20 @@ def get_tag(e):
             return k
 
 
-def parse(pyml):
+def parse(pyml, indent=0):
+    # print ("A", pyml)
+    if hasattr(pyml, 'lower'):  # raw text element
+        return "{0}{1}\n".format(" "*indent, pyml)
     html=""
     for el in pyml:
-        if hasattr(el, 'lower'):            #text element
-            html += el
-        else:
-            tag=get_tag(el)                 #tag element
-            body = el[tag]
-            html += "<{0}".format(tag)
-            for att, val in el.items():
-                if att != tag:
-                    html += ' {0}="{1}"'.format(att, val)
-            html += ">{0}</{1}>".format(parse(body), tag)
+        if hasattr(el, 'lower'):  # embedded text element
+            html += "{0}{1}\n".format(" "*indent, el)
+            continue
+        tag=get_tag(el)                 #tag element
+        body = el[tag]
+        html += "{0}<{1}".format(" "*indent, tag)
+        for att, val in el.items():
+            if att != tag:
+                html += ' {0}="{1}"'.format(att, val)
+        html += ">\n{0}{1}</{2}>\n".format(parse(body, indent=indent+INDENT), " "*indent, tag)
     return html
--- test.py
+++ test.py
@@ -1,15 +1,22 @@
 from pyweb import *
 from pprint import pprint
 
-pg=[
-    {html: [
+bod=[
+    {body: [
         "hello",
         {div: [
             {a: "link", 'href': "http://www.busyboxes.org", 'style': "font-family: sans; background-color: pink"}
         ]},
-        {span: "goodbye", 'style': "font-size: 200%"},
+        {span: "goodbye", 'style': "font-size: 200%; margin: 30px"},
         {"button": "OK", "onclick": "alert('okay')"}                                    #regular string is fine too
     ]}
 ]
 
-print(parse(pg))
+pyml=[
+    {html:[
+        {head: []},
+        {body: bod}
+    ]}
+]
+
+print(parse(pyml))
